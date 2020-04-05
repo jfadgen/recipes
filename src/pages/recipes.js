b66ac04e3b432152c5ac81bc22117c1395ce2d81
John Fadgen <jfadgen@customink.com>
2020-04-04 22:26:42 -0700

    Multiple recipes.

--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -1,19 +1,34 @@
 import React from "react"
-import YAMLData from "../../content/recipes/ginger_cookies.yml"
-const YAMLbuildtime = () => (
-  <div class="recipe-container">
-    <div class="recipe-header">
-      <h1>{YAMLData.title}</h1>
-      <div>From: <a href={YAMLData.reference}>{YAMLData.reference}</a></div>
-    </div>
+import RecipeData from "../../content/recipes.yml"
 
-    <div>{YAMLData.cooking_summary}</div>
-    <div>Ingredients:</div>
-    <ul>
-      {YAMLData.ingredients.map((data, index) => {
-        return <li key={`ingredients_item_${index}`}>{data.item}</li>
-      })}
-    </ul>
+const YAMLbuildtime = () => (
+  <div className="row">
+    <div className="col s12 m6">
+    {RecipeData.map(node => (
+      <Recipe key={node.id} recipe={node}/>
+    ))}
+    </div>
   </div>
 )
 export default YAMLbuildtime
+
+function Recipe(props) {
+  const recipe = props.recipe;
+  return (
+    <div className="card">
+      <div className="card-content ">
+        <span className="card-title">{recipe.title}</span>
+        <span>From: <a href={recipe.reference}>{recipe.reference}</a></span>
+
+        <div>{recipe.cooking_summary}</div>
+        <div className="recipe-section">Ingredients:
+          <ul className="recipe-list">
+            {recipe.ingredients.map((data, index) => {
+              return <li key={`ingredients_item_${index}`}>{data.item}</li>
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
